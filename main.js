f944056e765aa174503bbfd86862fda1decc9bd5
jojopedrorodrigues <jlabssistem@gmail.com>
2023-05-20 08:45:10 -0300

    segundo

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ function addNewTask(){
     var list = document.getElementById('list');
     var texto = document.getElementById('tarefa').value;
     var listItem = document.createElement('li');
+    if(texto === 0){
+        alert('Escreva algo!');
+    }
     listItem.className = 'list-item';
     var textElement = document.createTextNode(texto);
     listItem.appendChild(textElement);
     list.appendChild(listItem);
+    
 }
